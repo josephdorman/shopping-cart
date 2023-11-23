6227866eedea90b1049438c47144c63d8c8e4c62
Joseph Dorman <JDorman555@yahoo.com>
2023-11-23 01:47:18 -0600

    fetch data and display data through card component

---
+++ src/components/card.jsx
@@ -0,0 +1,21 @@
+import '../styles/card.css'
+
+function Card({ data }) {
+
+  return (
+    <>
+      {
+        data.map(data => (
+          <div key={data.id}>
+            <div>{data.id}</div>
+            <div>{data.description}</div>
+            <div>{data.category}</div>
+            <div>{data.price}</div>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export default Card
--- src/components/store.jsx
+++ src/components/store.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react'
+import Card from './card'
 import '../styles/store.css'
 
 function Store() {
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/category/jewelery', {mode: "cors"}) // to fetch men's clothing use "men's%20clothing"
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then(json => setData(json))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [])
+
   return (
     <>
       <p>THIS IS THE STORE PAGE</p>
+      {error 
+      ? <p>A network error was encountered</p> // if error render error
+      : loading ? <p>Loading...</p> // if no error check if loading and output loading if loading
+      : <Card data={data}/> // if not loading then render the card (data finished fetching)
+      }
     </>
   )
 }
