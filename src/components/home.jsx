b9b6d438759a13888177463511241c00c479a0b5
Joseph Dorman <JDorman555@yahoo.com>
2023-11-22 22:14:37 -0600

    setup temp code to test routing

---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import '../styles/home.css'
+
+function Home() {
+  return (
+    <>
+      <p>THIS IS THE HOME PAGE</p>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/store.jsx
@@ -0,0 +1,11 @@
+import '../styles/store.css'
+
+function Store() {
+  return (
+    <>
+      <p>THIS IS THE STORE PAGE</p>
+    </>
+  )
+}
+
+export default Store
