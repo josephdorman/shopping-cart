acc96c155fbe4616d5c8a7f7e6a1922ac9ea93b3
Joseph Dorman <JDorman555@yahoo.com>
2023-11-25 17:36:53 -0600

    import test functions

--- src/_tests_/store.test.jsx
+++ src/_tests_/store.test.jsx
@@ -1,2 +1,4 @@
-import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it } from 'vitest';
+import Store from '../components/store'
