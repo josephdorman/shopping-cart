e9e4ea4b616cc62a8f8109f3f2aaa232a09f6548
Joseph Dorman <JDorman555@yahoo.com>
2023-11-25 22:11:36 -0600

    add product info

--- src/components/productPage.jsx
+++ src/components/productPage.jsx
@@ -1,12 +1,28 @@
 import PropTypes from 'prop-types'
 import '../styles/productPage.css'
 
+ /* info in array
+  Category
+  Description
+  ID
+  Image
+  Price
+  Rating
+   Count
+   Rate
+  Title
+  */
+
 function ProductPage({ product }) {
   return (
     <>
       <div>THIS IS THE PRODUCT PAGE</div>
       <div key={product.id}>
         <p>{product.title}</p>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+        <p>{product.image}</p>
+        <p>{product.rating.rate} {product.rating.count}</p>
       </div>
     </>
   )
