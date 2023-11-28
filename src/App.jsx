173e02cebb8b14a31de4c4d682cb67120fde8efc
Joseph Dorman <JDorman555@yahoo.com>
2023-11-28 15:39:27 -0600

    style home page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <>
       <Header></Header>
-      <Outlet />
+      <div className='body'>
+        <Outlet />
+      </div>
     </>
   )
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,12 @@ import '../styles/home.css'
 function Home() {
   return (
     <>
-      <p>THIS IS THE HOME PAGE</p>
+      <div className='home'>
+        <h1 className='title'>HIGHRISE</h1>
+        <p className='catch'>STREETWEAR CLOTHING</p>
+        <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <button className='shop-now'>SHOP NOW</button>
+      </div>
     </>
   )
 }
