34c4ca30f50335e7abbc2b822e3e91d37e855841
Joseph Dorman <JDorman555@yahoo.com>
2023-11-28 22:34:55 -0600

    style product page

--- src/components/productPage.jsx
+++ src/components/productPage.jsx
@@ -16,14 +16,46 @@ import '../styles/productPage.css'
 function ProductPage({ product, onAddToCart }) {
   return (
     <>
-      <div>THIS IS THE PRODUCT PAGE</div>
-      <div key={product.id}>
-        <p>{product.title}</p>
-        <p>{product.description}</p>
-        <p>{product.price}</p>
-        <p>{product.image}</p>
-        <p>{product.rating.rate} {product.rating.count}</p>
-        <button onClick={() => onAddToCart(product)}>ADD TO CART</button>
+      <div key={product.id} className='product'>
+        <div className="product-img-container">
+          <img className="product-img" src={product.image} />
+        </div>
+        <div className="product-info">
+          <div className='product-main'>
+            <h2 className='product-title'>{product.title}</h2>
+            <p className='product-rating'>{product.rating.rate} {product.rating.count}</p>
+            <p className='product-price'>${product.price}</p>
+          </div>
+          <div className='product-color'>
+            <p>Color :</p>
+            <div className="button-container">
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+          </div>
+          <div className='product-size'>
+            <p>Size :</p>
+            <div className='button-container'>
+              <button>XXS</button>
+              <button>XS</button>
+              <button>S</button>
+              <button>M</button>
+              <button>L</button>
+              <button>XL</button>
+              <button>XXL</button>
+            </div>
+          </div>
+          <button className='add-cart' onClick={() => onAddToCart(product)}>ADD TO CART</button>
+          <div className='product-desc'>
+            <p className='desc-title'>Product Info</p>
+            <p className='desc'>{product.description}</p>
+          </div>
+        </div>
       </div>
     </>
   )
